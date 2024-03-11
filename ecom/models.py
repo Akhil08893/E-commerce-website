31bb02f8b5e3897177f83f5a20a9870fbadcf0ed
Akhil08893 <akhilyerubandi143@gmail.com>
2024-03-11 10:37:44 +0530

    changes

--- ecom/models.py
+++ ecom/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    description = models.CharField(max_length=150,null=True,default='',blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='uploads/product/')
+
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email =  models.EmailField(max_length=254)
+    phone = models.CharField(max_length=50)
+    passward = models.CharField(max_length=50)
+
+
+class Order(models.Model):
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer,on_delete=models.CASCADE)
+    address = models.CharField(max_length = 150)
+    phone = models.CharField(max_length = 150)
+    status = models.BooleanField(default=False)
+    data = models.DateField(auto_now=False, auto_now_add=True,blank=True)
+    
+    
+    
--- ecom/urls.py
+++ ecom/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from django.urls import path,include
 from . import views
-from . import settings
-from django.conf.url.static import static
 
 urlpatterns = [
     path('',views.home,name='home')
-] + static(settings.MEDIA_URL,document_root=MEDIA_ROOT)
+]
